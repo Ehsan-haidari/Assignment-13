8bb3107b5c0d63a289ac816b602ef9bde179da22
Ehsan <Ehsan.haidari6767@gamil.com>
2024-09-22 02:00:31 -0700

    Second commit

---
+++ js2.js
@@ -0,0 +1,9 @@
+function chick (number) {
+    if (number % 2 === 0) {
+        console.log(number + " is even.");
+    } else {
+        console.log(number + " is odd.");
+    }
+}
+chick (5);
+chick (12);
